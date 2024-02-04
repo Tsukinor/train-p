a3f071dd9f97989bb65a3e15d16c6844da146580
Jeffrey <894860131@qq.com>
2024-02-04 23:26:05 +0800

    4.4 修改启动日志,增加/member,修改logback日志，修改了.gitignore文件

--- member/src/main/java/com/jiawa/train/number/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/number/config/MemberApplication.java
@@ -1,8 +1,11 @@
 package com.jiawa.train.number.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 /**
  * @author liushuailong
@@ -13,7 +16,12 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.jiawa")
 public class MemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("测试地址: \thttp://127.0.0.1:{}/member/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 }
