b6bf903547b0d2eef75c1759aff755a5d0b05527
Jeffrey <894860131@qq.com>
2024-02-17 21:17:48 +0800

    4.15 实现一个简单的注册接口

--- member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.jiawa.train.member.controller;
 
 import com.jiawa.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
     @Resource
     private MemberService memberService;
-    @RequestMapping("/count")
+    @GetMapping("/count")
     public Integer count(){
-        return memberService.count();
+        return memberService. count();
+    }
+
+    @PostMapping("/register")
+    public long register(String mobile){
+        return memberService.register(mobile);
     }
 }
--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.jiawa.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.jiawa.train.member.domain.Member;
+import com.jiawa.train.member.domain.MemberExample;
 import com.jiawa.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,5 +17,24 @@ public class MemberService {
 
     public int count(){
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(String mobile){
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        if (CollUtil.isNotEmpty(list)){
+            throw new RuntimeException("手机号已注册");
+        }
+
+//        memberMapper.selectByExample(memberExample);
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
+
+
     }
 }
