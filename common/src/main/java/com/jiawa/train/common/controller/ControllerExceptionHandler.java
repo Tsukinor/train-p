bddb3337978579dd7213881c62d07837c1ea4e9f
Jeffrey <894860131@qq.com>
2024-02-17 22:53:06 +0800

    4.19 集成校验框架validation

--- common/src/main/java/com/jiawa/train/common/controller/ControllerExceptionHandler.java
+++ common/src/main/java/com/jiawa/train/common/controller/ControllerExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.springframework.validation.BindException;
+
 
 /**
  * 统一异常处理，数据预处理等
@@ -50,6 +52,19 @@ public class ControllerExceptionHandler {
         LOG.error("业务异常：{}", e.getE().getDesc());
         commonResp.setSuccess(false);
         commonResp.setMessage(e.getE().getDesc());
+        return commonResp;
+    } /**
+     * 校验异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = BindException.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(BindException e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("校验异常：{}",e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         return commonResp;
     }
 }
