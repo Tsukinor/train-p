2fbc223c6bdc534b20f50b9293e544ae67b3ac84
Jeffrey <894860131@qq.com>
2024-02-27 18:28:25 +0800

    5.7 增加短信验证码接口，加入LOG_ID

--- common/src/main/java/com/jiawa/train/common/aspect/LogAspect.java
+++ common/src/main/java/com/jiawa/train/common/aspect/LogAspect.java
@@ -1,6 +1,7 @@
 package com.jiawa.train.common.aspect;
 
 
+import cn.hutool.core.util.RandomUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.support.spring.PropertyPreFilters;
 import jakarta.servlet.ServletRequest;
@@ -15,6 +16,7 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -39,6 +41,7 @@ public class LogAspect {
     @Before("controllerPointcut()")
     public void doBefore(JoinPoint joinPoint) {
 
+        MDC.put("LOG_ID",System.currentTimeMillis()+ RandomUtil.randomString(3));
         // 开始打印请求日志
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
