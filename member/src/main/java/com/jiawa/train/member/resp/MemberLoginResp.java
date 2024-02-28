5780e2548101c8eef71793d00588337adb47b4b7
Jeffrey <894860131@qq.com>
2024-02-28 14:19:05 +0800

    6.4 封装hutool的JWT工具

--- member/src/main/java/com/jiawa/train/member/resp/MemberLoginResp.java
+++ member/src/main/java/com/jiawa/train/member/resp/MemberLoginResp.java
@@ -5,7 +5,7 @@ public class MemberLoginResp {
 
     private String mobile;
 
-//    private String token;
+    private String token;
 
     public Long getId() {
         return id;
@@ -23,19 +23,20 @@ public class MemberLoginResp {
         this.mobile = mobile;
     }
 
-//    public String getToken() {
-//        return token;
-//    }
+    public String getToken() {
+        return token;
+    }
 
-//    public void setToken(String token) {
-//        this.token = token;
-//    }
+    public void setToken(String token) {
+        this.token = token;
+    }
 
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("MemberLoginResp{");
         sb.append("id=").append(id);
         sb.append(", mobile='").append(mobile).append('\'');
+        sb.append(", token='").append(token).append('\'');
         sb.append('}');
         return sb.toString();
     }
--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -5,6 +5,7 @@ import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.jiawa.train.common.exception.BusinessException;
 import com.jiawa.train.common.exception.BusinessExceptionEnum;
+import com.jiawa.train.common.util.JwtUtil;
 import com.jiawa.train.common.util.SnowUtil;
 import com.jiawa.train.member.domain.Member;
 import com.jiawa.train.member.domain.MemberExample;
@@ -90,8 +91,10 @@ public class MemberService {
             throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_CODE_ERROR);
         }
 
-        return BeanUtil.copyProperties(memberDB,MemberLoginResp.class);
-
+        MemberLoginResp memberLoginResp = BeanUtil.copyProperties(memberDB,MemberLoginResp.class);
+        String token = JwtUtil.createToken(memberLoginResp.getId(),memberLoginResp.getMobile());
+        memberLoginResp.setToken(token);
+        return memberLoginResp;
     }
 
     private Member selectByMobile(String mobile) {
