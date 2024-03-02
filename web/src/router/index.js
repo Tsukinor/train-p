13ed5c717e8f4c937a4d6593b364c9be8412e239
Jeffrey <894860131@qq.com>
2024-03-02 16:14:33 +0800

    7.6 制作欢迎页面

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -15,7 +15,15 @@ const routes = [
     meta: {
       loginRequire: true
     },
-  }
+    children: [{
+      path: 'welcome',
+      component: () => import('../views/main/welcome.vue'),
+    }]
+  },
+  {
+    path: '',
+    redirect:'/welcome'
+  },
 
 ]
 
--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -6,7 +6,7 @@
       <a-layout-content
           :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
       >
-        所有会员总数：{{count}}
+<!--        所有会员总数：{{count}}-->
         <router-view></router-view>
       </a-layout-content>
     </a-layout>
@@ -15,9 +15,9 @@
 <script>
 import TheHeader from "@/components/the-header";
 import TheSider from "@/components/the-sider";
-import {defineComponent, ref} from "vue";
-import axios from "axios";
-import {notification} from "ant-design-vue";
+import {defineComponent} from "vue";
+// import axios from "axios";
+// import {notification} from "ant-design-vue";
 
 export default defineComponent({
   components:{
@@ -25,18 +25,18 @@ export default defineComponent({
     TheSider
   },
   setup() {
-    const count = ref(0);
+    // const count = ref(0);
 
-    axios.get("/member/member/count",).then(response => {
-      let data = response.data;
-      if (data.success) {
-        count.value = data.content;
-      } else {
-        notification.error({description: data.message});
-      }
-    })
+    // axios.get("/member/member/count",).then(response => {
+    //   let data = response.data;
+    //   if (data.success) {
+    //     count.value = data.content;
+    //   } else {
+    //     notification.error({description: data.message});
+    //   }
+    // })
   return {
-    count
+    // count
     };
   }
   });
