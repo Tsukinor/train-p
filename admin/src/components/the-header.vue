ff88af78fb4f8d4fc1177048f94f6bc86440f757
Jeffrey <894860131@qq.com>
2024-03-05 22:04:51 +0800

    9.3 修改admin路由及其配置

--- admin/src/components/the-header.vue
+++ admin/src/components/the-header.vue
@@ -3,12 +3,11 @@
     <div class="logo">
       <!--使用 router-link标签+to来跳转页面，相当于a标签+href-->
       <router-link to="/welcome" style="color: white; font-size: 18px">
-        12306铁路售票系统
+        Jeffrey12306铁路售票系统
       </router-link>
     </div>
     <div style="float: right; color: white;">
-      您好：{{ member.mobile }} &nbsp;&nbsp;
-      <a-button type="primary" @click="logout">退出登录</a-button>
+      欢迎使用控台管理
     </div>
     <a-menu
         v-model:selectedKeys="selectedKeys"
@@ -21,29 +20,9 @@
           <coffee-outlined/> &nbsp; 欢迎
         </router-link>
       </a-menu-item>
-      <a-menu-item key="/passenger">
-        <router-link to="/passenger">
-          <user-outlined/> &nbsp; 乘客管理
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/ticket">
-        <router-link to="/ticket">
-          <border-outer-outlined/> &nbsp; 余票查询
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/myticket">
-        <router-link to="/myticket">
-          <idcard-outlined/> &nbsp; 我的车票
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/seat">
-        <router-link to="/seat">
-          <usergroup-add-outlined/> &nbsp; 座位销售图
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/admin">
-        <router-link to="/admin">
-          <desktop-outlined/> &nbsp; 控制台管理
+      <a-menu-item key="/about">
+        <router-link to="/about">
+          <user-outlined/> &nbsp; 关于
         </router-link>
       </a-menu-item>
     </a-menu>
@@ -52,13 +31,11 @@
 
 <script>
 import {defineComponent, ref, watch} from 'vue';
-import store from "@/store";
 import router from "@/router";
 
 export default defineComponent({
   name: "ConsoleHeaderView",
   setup() {
-    let member = store.state.member;
     // ref和reactive都用于定义响应式变量
     // ref用来声明基本的数据类型，reactive用来声明对象或对象数组
     const selectedKeys = ref([])
@@ -67,14 +44,8 @@ export default defineComponent({
       selectedKeys.value = [];
       selectedKeys.value.push(newValue);
     }, {immediate: true});
-    const logout = () => {
-      store.commit("setMember", {})
-      router.push("/login");
-    };
     return {
-      member,
       selectedKeys,
-      logout
     }
   },
 });
--- admin/src/store/index.js
+++ admin/src/store/index.js
@@ -5,28 +5,17 @@
 *         sessionStorage是储存到关闭为止
 *         cookie不适合存大量数据。
 * */
-
 import { createStore } from 'vuex'
-
-// 定义sessionStorage中存储的key
-const MEMBER="member"
 
 export default createStore({
   // state内声明了数据对象，相当于Java的实体类
   state: {
-    // member创建时从sessionStorage中读取，若不存在则为空对象
-    member: window.SessionStorage.get(MEMBER)||{}
   },
   // 除非存在数值类型等转换，否则直接通过state.xxx访问即可
   getters: {
   },
   // mutations内说明了对数据对象的赋值操作，相当于Java的setter()
   mutations: {
-    setMember(state, _member){
-      // 设置store内的数据对象，同时刷新sessionStorage内的数据对象
-      state.member=_member
-      window.SessionStorage.set(MEMBER, _member);
-    }
   },
   // actions内声明异步任务
   actions: {
