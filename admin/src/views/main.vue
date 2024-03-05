5fb3bb845c24ae37291cd88a6406a30c7ade5377
Jeffrey <894860131@qq.com>
2024-03-05 21:36:53 +0800

    9.3 创建admin模块，所有代码都从web模块复制

---
+++ admin/src/views/main.vue
@@ -0,0 +1,46 @@
+<template>
+  <a-layout id="components-layout-demo-top-side-2">
+    <the-header></the-header>
+    <a-layout>
+      <the-sider></the-sider>
+      <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+      >
+<!--        所有会员总数：{{count}}-->
+        <router-view></router-view>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+import TheHeader from "@/components/the-header";
+import TheSider from "@/components/the-sider";
+import {defineComponent} from "vue";
+// import axios from "axios";
+// import {notification} from "ant-design-vue";
+
+export default defineComponent({
+  components:{
+    TheHeader,
+    TheSider
+  },
+  setup() {
+    // const count = ref(0);
+
+    // axios.get("/member/member/count",).then(response => {
+    //   let data = response.data;
+    //   if (data.success) {
+    //     count.value = data.content;
+    //   } else {
+    //     notification.error({description: data.message});
+    //   }
+    // })
+  return {
+    // count
+    };
+  }
+  });
+</script>
+<style scoped>
+
+</style>
---
+++ admin/src/views/main/my-ticket.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "my-ticket"
+}
+</script>
+
+<style scoped>
+
+</style>
